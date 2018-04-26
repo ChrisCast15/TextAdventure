1f8d89795fca509ab291373034810bbcc48a2f2e
Christian Castillo <castilc4180@student.laccd.edu>
2018-04-26 15:19:35 -0700

    Added getInt to utilities.InputHelper

--- src/com/gamedriver/java/utilities/InputHelper.java
+++ src/com/gamedriver/java/utilities/InputHelper.java
@@ -1,14 +1,31 @@
 package com.gamedriver.java.utilities;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputHelper {
 
+    //asks a prompt and awaits input. returns String
     public static String getInput(String prompt) {
         System.out.print(prompt);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
+
+    //asks a prompt and waits for input
+    public static int getInt(String prompt) {
+        try {
+            System.out.print(prompt);
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Please input an integer!");
+            InputHelper.getInt(prompt)
+        }
+
+    }
+
+
 
 
 }
